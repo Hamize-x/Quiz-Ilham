37d6af9092d7cf81073ed093284ab819036f37cd
Ilham Agustiano Haneadi <ilhamagustian1408@gmail.com>
2024-10-31 09:42:40 +0700

    Add files via upload

---
+++ EnergiKinetik.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void hitungEK(double massa, double kecepatan) {
+	double energiKinetik = 0.5 * massa * kecepatan * kecepatan;
+	cout << "Hasil energi kinetiknya: " << energiKinetik << " joule" << endl;
+}
+int main() {
+	double massa, kecepatan;
+	cout << "Masukkan massa (kg): ";
+	cin >> massa;
+	cout << "Masukkan kecepatan (m/s): ";
+	cin >> kecepatan;
+
+	hitungEK(massa, kecepatan);
+	return 0;
+}
